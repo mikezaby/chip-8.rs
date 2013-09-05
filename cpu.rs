e87f01f246f1db69a44b09b8433f793097db1fbc
mikezaby <mikezaby@gmail.com>
2013-09-06 01:18:46 +0300

    add basic structure for executing opcodes

--- cpu.rs
+++ cpu.rs
@@ -32,11 +32,51 @@ impl Cpu {
 
   pub fn emulate_cycle(&mut self) {
     self.fetch_opcode();
+    self.opcode_execute();
   }
 
   fn fetch_opcode(&mut self) {
     self.opcode = (self.memory[self.pc_reg] << 8 | self.memory[self.pc_reg + 1]) as u16;
   }
+
+  fn opcode_execute(&self) {
+    match (self.opcode & 0xf000) {
+      0x0000 => self.op_0xxx(),
+      0x1000 => self.op_1xxx(),
+      0x2000 => self.op_2xxx(),
+      0x3000 => self.op_3xxx(),
+      0x4000 => self.op_4xxx(),
+      0x5000 => self.op_5xxx(),
+      0x6000 => self.op_6xxx(),
+      0x7000 => self.op_7xxx(),
+      0x8000 => self.op_8xxx(),
+      0x9000 => self.op_9xxx(),
+      0xA000 => self.op_Axxx(),
+      0xB000 => self.op_Bxxx(),
+      0xC000 => self.op_Cxxx(),
+      0xD000 => self.op_Dxxx(),
+      0xE000 => self.op_Exxx(),
+      0xF000 => self.op_Fxxx(),
+      _      => println(fmt!("dunno"))
+    }
+  }
+
+  fn op_0xxx(&self) {}
+  fn op_1xxx(&self) {}
+  fn op_2xxx(&self) {}
+  fn op_3xxx(&self) {}
+  fn op_4xxx(&self) {}
+  fn op_5xxx(&self) {}
+  fn op_6xxx(&self) {}
+  fn op_7xxx(&self) {}
+  fn op_8xxx(&self) {}
+  fn op_9xxx(&self) {}
+  fn op_Axxx(&self) {}
+  fn op_Bxxx(&self) {}
+  fn op_Cxxx(&self) {}
+  fn op_Dxxx(&self) {}
+  fn op_Exxx(&self) {}
+  fn op_Fxxx(&self) {}
 }
 
 static fontset: [u8, ..80] = [0xF0, 0x90, 0x90, 0x90, 0xF0, 0x20, 0x60, 0x20, 0x20, 0x70,
