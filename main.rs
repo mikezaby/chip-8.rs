9b37595a013d2d30bc6cab54b7828bc4f7f3a409
mikezaby <mikezaby@gmail.com>
2013-09-15 00:18:16 +0300

    get game that should be load from keyboard

--- main.rs
+++ main.rs
@@ -1,10 +1,13 @@
 use cpu::Cpu;
+use std::io;
 
 pub mod cpu;
 
 fn main() {
   let mut cpu = Cpu::new();
-  cpu.load_game(~"games/ibm");
+  let game = io::stdin().read_line();
+  cpu.load_game(~"games/" + game);
+
   loop {
     cpu.emulate_cycle();
     cpu.display.draw_screen();
